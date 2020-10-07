5b9eefcdc97064e4e6aae20a8e722fb0c191c69d
Dmitriy Filippov <dmt.filippov@gmail.com>
2020-10-07 23:58:06 +0700

    function trimSymbols

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -4,6 +4,21 @@
  * @param {number} size - the allowed size of consecutive identical symbols
  * @returns {string} - the new string without extra symbols according passed size
  */
-export function trimSymbols(string, size) {
 
+export function trimSymbols(string, size) {
+  const stringArr = string.split('');
+  let result = '';
+  let tempStr = '';
+  //работаетб но я еще подумаю над этим
+  for (const char of stringArr) {
+    if ( (tempStr[0] === char)) {
+      tempStr += char;
+    } else if (tempStr[0] !== char) {
+      tempStr = char;
+    }
+    if (!(tempStr.length > size)) {
+      result += char;
+    }
+  }
+  return result;
 }
