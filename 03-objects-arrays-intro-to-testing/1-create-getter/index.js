e04503755ad530f6ef6d13d554b2eca077caa1ec
Dmitriy Filippov <dmt.filippov@gmail.com>
2020-10-07 22:49:48 +0700

    function createGetter

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,19 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
+  const pathArr = path.split('.');
 
+  // тесты проходит, но мне кажется не лучший способ
+  return function (obj) {
+    let result = obj;
+    for (const pathstr of pathArr) {
+      if(Object.keys(result).includes(pathstr)) {
+        result = result[pathstr];
+      } else {
+        result = undefined;
+        break;
+      }
+    }
+    return result;
+  }
 }
